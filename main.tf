fec213730a04c798e8ee627c804f72ac9b00bae6
Aiman Md Uslim <aimanuslim@gmail.com>
2022-10-06 10:27:28 +0800

    Remove secrets

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-
-/*{
-  "appId": "bcd0ac8c-c29b-44ee-9107-317260fdb991",
-  "displayName": "azure-cli-2022-10-03-02-35-40",
-  "password": "aU18Q~4pTvDDcjmRAL1oVzdSVHQRr.U0tGtnUbuU",
-  "tenant": "6ca458d8-2f10-4cd2-a2d7-c5db3e6eacf7"
-}*/
-
 terraform {
   required_providers {
     azurerm = {
@@ -20,10 +12,6 @@ provider "azurerm" {
   features {}
 
   alias           = "dev"
-  subscription_id = "5e0b5cdf-a97f-4e0d-b8b6-5ca78f448046"
-  client_id       = "bcd0ac8c-c29b-44ee-9107-317260fdb991"
-  client_secret   = "aU18Q~4pTvDDcjmRAL1oVzdSVHQRr.U0tGtnUbuU"
-  tenant_id       = "6ca458d8-2f10-4cd2-a2d7-c5db3e6eacf7"
 }
 
 resource "azurerm_resource_group" "example" {
